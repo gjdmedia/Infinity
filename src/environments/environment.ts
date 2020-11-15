4cd011be4dd2f835a353fa4d85e32f2859aeac2e
Gareth Day <garethjday@gmail.com>
2020-11-15 15:47:38 +0000

    Store Deployment 1.01

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDzXROVL0ql6_SjloAmaU_a4M-bijJI5h4",
-    authDomain: "infinity-generator-a8f32.firebaseapp.com",
-    databaseURL: "https://infinity-generator-a8f32.firebaseio.com",
-    projectId: "infinity-generator-a8f32",
-    storageBucket: "infinity-generator-a8f32.appspot.com",
-    messagingSenderId: "728926959507",
-    appId: "1:728926959507:web:a5c8f02c4344f2eae59a66",
-    measurementId: "G-SFNL15EPF8"
+    apiKey: "AIzaSyA8C3UCup5woYmKF6dUhf0L2lLWkpy6X4Q",
+  authDomain: "infinity-generator-9dd24.firebaseapp.com",
+  databaseURL: "https://infinity-generator-9dd24.firebaseio.com",
+  projectId: "infinity-generator-9dd24",
+  storageBucket: "infinity-generator-9dd24.appspot.com",
+  messagingSenderId: "946195882953",
+  appId: "1:946195882953:web:672eb24d0c52aefc8afe18",
+  measurementId: "G-43VCWT3QRP"
   }
 };
 
